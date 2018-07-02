b50721bd5b1d9fbb5ba13c2ed8e8cd2d09b1d3aa
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-07-01 23:07:39 -0400

    Version 0.0.0.1
    feat: Adds logic for menu  selection
    feat: Adds handlbars interaction

--- scripts/clickLogic.js
+++ scripts/clickLogic.js
@@ -1,21 +1,54 @@
 $(document).ready(function () {
   
 
-    let cats=[{
+    var cats=[
+        {
         name: 'Pedro',
         url:"https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-    }];
-    var  katItemTemplate = Handlebars.compile($('#katMenuItem').html());// Used to call the template into a variable
-    katList = $('.katList');// Used as an object to append to
-katList.append(katItemTemplate(cats[0]))////Makes the conversion 
-  
+    },
+    {
+        name: 'Juan',
+        url:"https://www.pexels.com/photo/animal-cat-face-close-up-feline-416160/"
+    },
+    {
+        name: 'Jose',
+        url:"https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?cs=srgb&dl=adorable-animal-blur-617278.jpg&fm=jpg"
+    },
+    {
+        name: 'Pedro Jr',
+        url:"https://images.pexels.com/photos/17767/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+
+
+];
+    var  katItemTemplate = Handlebars.compile($('#katMenuItem').html()),// Used to call the template into a variable
+     katDisplayTemplate = Handlebars.compile($('#katContent').html()),
+     katList = $('.katList'),
+     katDisplay= $('.kat-container');// Used as an object to append to
+cats.forEach(
+    function(cat){
+        katList.append(katItemTemplate(cat));
+    }
+
+)
+katList.on('click','li',function(){ 
+var item =$(this),indexNumber=0;
+console.log(item);
+console.log(cats.findIndex(ind=> ind.name==item[0].children[0].innerText));
+indexNumber=cats.findIndex(ind=> ind.name==item[0].children[0].innerText);//Gets the clicked element's name 
+katDisplay.empty();//Clears Display area
+katDisplay.append(katDisplayTemplate(cats[indexNumber]));//Changes the info
+});
   
 })
+
+
 class Cat {
         constructor(name,url) {
         this.name = name;
         this.picture = url;
         this.cout = 0;
+        this.id;
     }
     countUp()
     {
