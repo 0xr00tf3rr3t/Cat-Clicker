2a389dbb5f45ceac2049b1a9d6ea39e21f10ce64
Michael Valentin <Michael.Valentin1136@gmail.com>
2018-07-02 21:33:24 -0400

    Version 1.0.0.0
    Feat: Clicking increment ther count of cats.
    feat: Can Switch between cats.
    feat: each one is different

--- scripts/clickLogic.js
+++ scripts/clickLogic.js
@@ -1,60 +1,50 @@
 $(document).ready(function () {
-  
 
-    var cats=[
-        {
-        name: 'Pedro',
-        url:"https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-    },
-    {
-        name: 'Juan',
-        url:"https://www.pexels.com/photo/animal-cat-face-close-up-feline-416160/"
-    },
-    {
-        name: 'Jose',
-        url:"https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?cs=srgb&dl=adorable-animal-blur-617278.jpg&fm=jpg"
-    },
-    {
-        name: 'Pedro Jr',
-        url:"https://images.pexels.com/photos/17767/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-    },
+    var cats = [];// Holds the all the cats 
+    var indexNumber = 0;//This was made avaaible globaly for easier managment
+    cats.push(new Cat('Pedro', 'imgs/0.jpg'));//Initializing 4 cats
+    cats.push(new Cat('Juan', 'imgs/1.jpg'));//
+    cats.push(new Cat('Jose', 'imgs/2.jpg'));//
+    cats.push(new Cat('Junior', 'imgs/3.jpg'));//
 
+    var katItemTemplate = Handlebars.compile($('#katMenuItem').html()), // Used to call the template into a variable
+        katDisplayTemplate = Handlebars.compile($('#katContent').html()),
+        katList = $('.katList'),//Holds the menu element
+        katDisplay = $('.kat-container'); // Used as an object to append the  actual info of cat
 
-];
-    var  katItemTemplate = Handlebars.compile($('#katMenuItem').html()),// Used to call the template into a variable
-     katDisplayTemplate = Handlebars.compile($('#katContent').html()),
-     katList = $('.katList'),
-     katDisplay= $('.kat-container');// Used as an object to append to
-cats.forEach(
-    function(cat){
-        katList.append(katItemTemplate(cat));
-    }
+    cats.forEach(
+        function (cat) {
+            katList.append(katItemTemplate(cat));//Loads the cats that are already inside the Array
+        }
 
-)
-katList.on('click','li',function(){ 
-var item =$(this),indexNumber=0;
-console.log(item);
-console.log(cats.findIndex(ind=> ind.name==item[0].children[0].innerText));
-indexNumber=cats.findIndex(ind=> ind.name==item[0].children[0].innerText);//Gets the clicked element's name 
-katDisplay.empty();//Clears Display area
-katDisplay.append(katDisplayTemplate(cats[indexNumber]));//Changes the info
-});
-  
+    )
+    katList.on('click', 'li', function () {
+        var item = $(this);//Used to identify the elements that are clicked
+        indexNumber = cats.findIndex(ind => ind.name == item[0].children[0].innerText); //Gets the clicked element's name 
+        katDisplay.empty(); //Clears Display area
+        katDisplay.append(katDisplayTemplate(cats[indexNumber])); //Changes the info
+        katDisplay.unbind();//unbinds any 'this' value that had been binded into katDisplay
+        katDisplay.on('click','img',cats[indexNumber].countUp.bind(cats[indexNumber]));//Binds the this element with the one selected
+    });
+   
 })
 
 
 class Cat {
-        constructor(name,url) {
+     
+    constructor(name, url) {
         this.name = name;
-        this.picture = url;
-        this.cout = 0;
-        this.id;
-    }
-    countUp()
-    {
+        this.url = url;
+        this.count = 0;
+      
+
+    };
+     countUp() {
+             
         this.count++;
+        $('.clicksNumber')[0].innerText=this.count;
+        console.log(this);
+        console.log(this.count);
+
     }
-
-
-
 }
